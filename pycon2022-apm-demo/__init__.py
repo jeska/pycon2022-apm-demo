e47f9e00552fdb9a1379f84f9343cfa76e39f4bb
Jessica David <jessica.david@elastic.co>
2022-04-30 11:12:43 -0600

    added some samples

--- pycon2022-apm-demo/__init__.py
+++ pycon2022-apm-demo/__init__.py
@@ -1,18 +1,35 @@
+import logging
 import os
 
 from elasticapm.contrib.flask import ElasticAPM
 from flask import Flask
 
 app = Flask(__name__)
-app.config['ELASTIC_APM'] = {
-    'SERVICE_NAME': 'pycon2022-apm-demo',
-    'SECRET_TOKEN': os.environ.get("APM_SECRET_TOKEN"),
-    'SERVER_URL': os.environ.get("APM_SERVICE_URL"),
-    'ENVIRONMENT': 'demo',
+app.config["ELASTIC_APM"] = {
+    "SERVICE_NAME": "pycon2022-apm-demo",
+    "SECRET_TOKEN": os.environ.get("APM_SECRET_TOKEN"),
+    "SERVER_URL": os.environ.get("APM_SERVICE_URL"),
+    "ENVIRONMENT": "demo",
 }
-apm = ElasticAPM(app)
+apm = ElasticAPM(app, logging=logging.DEBUG)
 
 
-@app.route('/')
+@app.route("/")
 def hello_pycon():
-    return 'Hello, Pycon!'
+    return "Hello, Pycon!"
+
+
+@app.route("/logger")
+def hello_logger():
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        app.logger.error("This is a pycon error", exc_info=True)
+    finally:
+        return "We've logged an error!"
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.error(f"This page doesn't exist, Pycon: {error}", exc_info=True)
+    return "Page not found, try again."
